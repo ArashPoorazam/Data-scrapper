adf35240b54414cc51ba017eb6baaafdb5f5095e
ArashPoorazam <arashpoorazam88@gmail.com>
2025-03-12 08:39:00 +0330

    Optimization

--- main.py
+++ main.py
@@ -12,96 +12,80 @@ import Description as Des
 import Config as Keys
 import Responses as Res
 
-
 # Configure the logger
 logging.basicConfig(
-    level=logging.INFO,                                             
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("bot.log"),                             
-        logging.StreamHandler()                                     
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
     ]
 )
-
 
 # Create a bot object
 state_storage = StateMemoryStorage()
 bot = telebot.TeleBot(Keys.API_KEY, state_storage=state_storage)
 logging.info('Start bot...')
 
-
 # start command
 @bot.message_handler(commands=['start'])
 def start_command(user_message):
     key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     key_markup.add(telebot.types.KeyboardButton("🔎 سرچ 🔍"))
-
     bot.send_message(chat_id=user_message.chat.id, text=Des.start_description, reply_markup=key_markup)
-
 
 # search
 @bot.message_handler(func=lambda message: message.text == "🔎 سرچ 🔍")
 def search(user_message):
-    bot.send_message(chat_id=user_message.chat.id , text="🔍 نام محصول مورد نظرتان را بنویسید 🔻")
+    bot.send_message(chat_id=user_message.chat.id, text="🔍 نام محصول مورد نظرتان را بنویسید 🔻")
     bot.register_next_step_handler(user_message, scraping_data)
 
+# Helper function to extract price
+def extract_price(price_tag):
+    if len(price_tag) == 2:     # 100 تومان
+        return price_tag[0]
+    elif len(price_tag) == 3:   # از 100 تومان
+        return price_tag[1]
+    else:                       # 0
+        return "0"
 
 # scraping
 def scraping_data(user_message):
-    # dynamic url
-    search_term = user_message.text
-    search_term = search_term.replace(" ", "%20")
-    url = f"https://torob.com/search/?query={search_term}&available=true" # {page}{stock_status}{available}{sort}
+    search_term = user_message.text.replace(" ", "%20")
+    url = f"https://torob.com/search/?query={search_term}&available=true"
     response = requests.get(url)
 
-    # respond, should be 200
     logging.info(response)
 
     if response.status_code == 200:
-        # get the html text
         soup = BeautifulSoup(response.text, "html.parser")
         names = soup.find_all("h2", class_="ProductCard_desktop_product-name__JwqeK")
 
-        # save the found items name, price, and link into dictionary
-        items_found = {} 
+        items_found = {}
         for name in names[:10]:  # Limit to the first 10 items
-            # find the parents name
-            parent = name.parent
+            try:
+                parent = name.parent
+                link = parent.parent.get('href', '')
+                price_tag = parent.find("div", class_="ProductCard_desktop_product-price-text__y20OV").string.split(" ")
+                price = extract_price(price_tag)
+                items_found[name.string] = {
+                    'price': int(price.replace("٫", "")),
+                    'link': link
+                }
+            except AttributeError as e:
+                logging.error(f"Error parsing item: {e}")
+                continue
 
-            # find direct link from parent of parent
-            link = parent.parent.get('href', '')
-
-            # find the price from the parent
-            price_tag = parent.find("div", class_="ProductCard_desktop_product-price-text__y20OV").string.split(" ")
-
-            if len(price_tag) == 2:     # 100 تومان
-                price = price_tag[0]
-            elif len(price_tag) == 3:   # از 100 تومان
-                price = price_tag[1]
-            else:                       # 0
-                price = "0"
-            
-            # save name, price, and link in a dictionary
-            items_found[name.string] = {
-                'price': int(price.replace("٫", "")),
-                'link': link
-            }
-
-        # Format the items_found dictionary into a readable string with links
         message = "\n".join([f"{index + 1}. نام: <a href='https://torob.com{item['link']}'>{name}</a>\nقیمت: {item['price']} تومان💵\n\n" for index, (name, item) in enumerate(items_found.items())])
-
-        # Send the message with HTML parsing enabled
         bot.send_message(chat_id=user_message.chat.id, text=message, parse_mode='HTML')
     else:
         bot.reply_to(user_message, "Error in fetching data!")
-
 
 # Message response
 @bot.message_handler()
 def message_response(user_message):
     response = Res.sample_responses(user_message)
     bot.send_message(user_message.chat.id, response)
-
 
 if __name__ == '__main__':
     bot.add_custom_filter(custom_filters.StateFilter(bot))
